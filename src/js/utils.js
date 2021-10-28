c1305179a6dcdceab32e90bbb0780173b65fccbf
best-wangjia <wangjia125513819@foxmail.com>
2021-10-28 23:06:27 +0800

    modify: add code category

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,294 @@
 'use strict'
 
+// 补零
+function fillZero(val) {
+  return val < 10 ? '0' + val : val
+}
+// 获取随机数
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+/* string */
+
+// 首字母大写
+export function firstUpperCase(str) {
+  return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase())
+}
+// 随机ID
+export function randomID() {
+  return Math.random().toString(36).substring(2)
+}
+// 随机生成颜色码
+export function getRandomColor() {
+  return '#' + (function (h) {
+    return new Array(7 - h.length).join('0') + h
+  })((Math.random() * 0x1000000 << 0).toString(16))
+}
+// uuid
+export function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    let r = Math.random() * 16 | 0,
+        v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
+}
+// 获得字符长度
+export function getByteLen(val) {
+  let len = 0
+  for (let i = 0, l = val.length; i < l; i++) {
+    let a = val.charAt(i)
+    if (a.match(/[^\x00-\xff]/ig) != null) {
+      len += 2
+    } else {
+      len += 1
+    }
+  }
+  return len
+}
+// 随机字符串生成
+export function randomString(len) {
+  len = len || 32;
+  let $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+  let maxPos = $chars.length
+  let str = ''
+  for (let i = 0; i < len; i++) {
+    str += $chars.charAt(Math.floor(Math.random() * maxPos))
+  }
+  return str
+}
+// 驼峰转字符串
+export function fromCamelCase(str, separator = ' ') {
+  return str.replace(/([a-zd])([A-Z])/g, '$1' + separator + '$2')
+  .replace(/([A-Z]+)([A-Z][a-zd]+)/g, '$1' + separator + '$2').toLowerCase()
+}
+// 字符串转驼峰
+export function toCamelCase(str) {
+  let s = str.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g).map(x => x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase()).join('')
+  return s.slice(0, 1).toLowerCase() + s.slice(1)
+}
+
+/* number */
+
+// 千分位数字
+export function thousands(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+// 匹配整数
+export function isInteger(val) {
+  return /^(-|\+)?\d+$/.test(val)
+}
+
+/* array */
+
+// 数组乱序
+export function shuffle(arr) {
+  let _arr = arr.slice()
+  for (let i = 0, l = _arr.length; i < l; i++) {
+    let j = getRandom(0, i), t = _arr[i]
+    _arr[i] = _arr[j]
+    _arr[j] = t
+  }
+  return _arr
+}
+// 数组求和
+export function arraySum(arr) {
+  if (!arr.length) {
+    return 0
+  } else if (arr.length === 1) {
+    return arr[0] - 0
+  } else {
+    return arr[0] - 0 + arraySum(arr.slice(1))
+  }
+}
+// 求数组最大值
+export function arrayMax(arr) {
+  return Math.max(...arr)
+}
+// 求数组最小值
+export function arrayMin(arr) {
+  return Math.min(...arr)
+}
+// 数组切割
+export function arrayChunk(arr, size) {
+  return Array.from({length: Math.ceil(arr.length / size)}, (v, i) => arr.slice(i * size, i * size + size))
+}
+// 过滤数组中false值
+export function arrayCompact(arr) {
+  return arr.filter(Boolean)
+}
+// 计算数组中值的出现次数
+export function arrayCountOccurrences(arr, value) {
+  return arr.reduce((a, v) => v === value ? a + 1 : a + 0, 0)
+}
+// 深度拼合数组
+export function deepFlatten(arr) {
+  return [].concat(...arr.map(v => Array.isArray(v) ? deepFlatten(v) : v))
+}
+// 返回两个数组间的差异
+export function arrayDiff(a, b) {
+  const s = new Set(b)
+  return a.filter(v => !s.has(v))
+}
+// 初始化指定范围内的数字数组
+export function initArrayWithRange(end, start = 0) {
+  return Array.from({length: end - start + 1}).map((v, i) => i + start)
+}
+
+/* date */
+
+// 时间转毫秒数
+export function getTime(time) {
+  return +new Date(`2001/01/01 ${time}`) - +new Date('2001/01/01 00:00:00')
+}
+// 毫秒数转时间
+export function getMSFormat(ms) {
+  if (ms >= 1000) {
+    let total = ms / 1000 // 总秒数
+      , day = parseInt(total / (24 * 60 * 60)) //计算整数天数
+      , afterDay = total - day * 24 * 60 * 60 //取得算出天数后剩余的秒数
+      , hour = parseInt(afterDay / (60 * 60)) //计算整数小时数
+      , afterHour = total - day * 24 * 60 * 60 - hour * 60 * 60 //取得算出小时数后剩余的秒数
+      , min = parseInt(afterHour / 60) //计算整数分
+      , afterMin = Math.floor(total - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60) //取得算出分后剩余的秒数
+      , hh = 0
+    if (day) {
+      hh = day * 24
+    }
+    hh += hour
+    return `${fillZero(hh)}:${fillZero(min)}:${fillZero(afterMin)}`
+  } else {
+    return `00:00:00`
+  }
+}
+// 获取某月的天数
+export function getDays(year, month) {
+  return new Date(year, month, 0).getDate()
+}
+// 获取某月某日是周几
+export function getWeekends(year, month, i) {
+  return new Date(year, month - 1, i).getDay()
+}
+// 时间戳转字符串
+export function timeToStr(timestamp, type = 'time') {
+  if (!timestamp) return '-'
+  let dt = new Date(timestamp)
+    , year = dt.getFullYear()
+    , month = fillZero(dt.getMonth() + 1)
+    , day = fillZero(dt.getDate())
+    , hour = fillZero(dt.getHours())
+    , minute = fillZero(dt.getMinutes())
+    , second = fillZero(dt.getSeconds())
+  if (type === 'time') {
+    return `${year}-${month}-${day} ${hour}:${minute}:${second}`
+  } else if (type === 'date') {
+    return `${year}-${month}-${day}`
+  }
+}
+// 计算两个日期之间相差天数，xxxx-xx-xx格式
+export function dateDiff(startDate, endDate) {
+  let startTime = new Date(Date.parse(startDate.replace(/-/g, '/'))).getTime()
+    , endTime = new Date(Date.parse(endDate.replace(/-/g, '/'))).getTime()
+    , dates = Math.abs((startTime - endTime)) / (1000 * 60 * 60 * 24)
+  return dates
+}
+// 时间戳转化为多久前
+export function timeFormat(timestamp) {
+  let mistiming = Math.round((Date.now() - timestamp) / 1000)
+    , arrr = ['年', '个月', '星期', '天', '小时', '分钟', '秒']
+    , arrn = [31536000, 2592000, 604800, 86400, 3600, 60, 1]
+  for (let i = 0; i < arrn.length; i++) {
+    let inm = Math.floor(mistiming / arrn[i])
+    if (inm != 0) {
+      return inm + arrr[i] + '前'
+    }
+  }
+}
+// 倒计时
+export function getCountDown(date) {
+  let ms = (Date.parse(date)) - (new Date().getTime())
+  if (ms >= 0) {
+    return {
+      'dd': Math.floor(ms / (1000 * 60 * 60 * 24)),
+      'hh': fillZero(Math.floor(ms / (1000 * 60 * 60)) % 24),
+      'mm': fillZero(Math.floor(ms / (1000 * 60)) % 60),
+      'ss': fillZero(Math.floor(ms / 1000) % 60)
+    };
+  } else {
+    return false
+  }
+}
+// 日期格式化
+Date.prototype.Format = function (format) {
+  let o = {
+    'M+': this.getMonth() + 1, // month
+    'd+': this.getDate(), // day
+    'h+': this.getHours(), // hour
+    'm+': this.getMinutes(), // minute
+    's+': this.getSeconds(), // second
+    'q+': Math.floor((this.getMonth() + 3) / 3), // quarter
+    'S': this.getMilliseconds() // millisecond
+  }
+  if (/(y+)/.test(format)) {
+    format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+  for (let k in o) {
+    if (new RegExp('(' + k + ')').test(format)) {
+      format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length))
+    }
+  }
+  return format
+}
+
+/* dom */
+
+// 获取窗口可视范围的高度
+export function getClientHeight() {
+  let clientHeight = 0, doc = document
+  if (doc.body.clientHeight && doc.documentElement.clientHeight) {
+    clientHeight = (doc.body.clientHeight < doc.documentElement.clientHeight) ? doc.body.clientHeight : doc.documentElement.clientHeight
+  } else {
+    clientHeight = (doc.body.clientHeight > doc.documentElement.clientHeight) ? doc.body.clientHeight : doc.documentElement.clientHeight
+  }
+  return clientHeight
+}
+// 获取滚动条距顶部高度
+export function getScrollTop() {
+  return document.documentElement.scrollTop || document.body.scrollTop
+}
+// 设置滚动条距顶部高度
+export function setScrollTop(y) {
+  window.scrollTo(0, y)
+}
+// 获取当前滚动条位置
+export function getScrollPosition() {
+  let win = window
+  return {
+    x: win.pageXOffset !== undefined ? win.pageXOffset : win.scrollLeft,
+    y: win.pageYOffset !== undefined ? win.pageYOffset : win.scrollTop
+  }
+}
+// 滚动到指定元素区域
+export function smoothScroll(element) {
+  document.querySelector(element).scrollIntoView({
+    behavior: 'smooth'
+  })
+}
+// 平滑滚动到页面顶部
+export function scrollToTop() {
+  const top = document.documentElement.scrollTop || document.body.scrollTop
+  if (top > 0) {
+    window.requestAnimationFrame(scrollToTop)
+    window.scrollTo(0, top - top / 8)
+  }
+}
+// 判断元素是否在视口中
+export function elementIsVisibleInViewport(el) {
+  const { top, left, bottom, right } = el.getBoundingClientRect()
+  const { innerHeight, innerWidth } = window
+  return ((top > 0 && top < innerHeight) || (bottom > 0 && bottom < innerHeight)) && ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
+}
+
 // 多继承
 export function mix(...mixins) {
   function copyProperties(target, source) {
@@ -153,99 +442,9 @@ export function setParams(url, param) {
   link = url + '?' + args.substr(1)
   return link.replace(' ', '')
 }
-// 首字母大写
-export function firstUpperCase(str) {
-  return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase())
-}
-// 补零
-function fillZero(val) {
-  return val < 10 ? '0' + val : val
-}
 // sleep
 export function sleep(delay) {
   return new Promise((resolve, reject) => setTimeout(resolve, delay))
-}
-// 时间转毫秒数
-export function getTime(time) {
-  return +new Date(`2001/01/01 ${time}`) - +new Date('2001/01/01 00:00:00')
-}
-// 毫秒数转时间
-export function getMSFormat(ms) {
-  if (ms >= 1000) {
-    let total = ms / 1000 // 总秒数
-      , day = parseInt(total / (24 * 60 * 60)) //计算整数天数
-      , afterDay = total - day * 24 * 60 * 60 //取得算出天数后剩余的秒数
-      , hour = parseInt(afterDay / (60 * 60)) //计算整数小时数
-      , afterHour = total - day * 24 * 60 * 60 - hour * 60 * 60 //取得算出小时数后剩余的秒数
-      , min = parseInt(afterHour / 60) //计算整数分
-      , afterMin = Math.floor(total - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60) //取得算出分后剩余的秒数
-      , hh = 0
-    if (day) {
-      hh = day * 24
-    }
-    hh += hour
-    return `${fillZero(hh)}:${fillZero(min)}:${fillZero(afterMin)}`
-  } else {
-    return `00:00:00`
-  }
-}
-// 获取某月的天数
-export function getDays(year, month) {
-  return new Date(year, month, 0).getDate()
-}
-// 获取某月某日是周几
-export function getWeekends(year, month, i) {
-  return new Date(year, month - 1, i).getDay()
-}
-// 倒计时
-export function getCountDown(date) {
-  let ms = (Date.parse(date)) - (new Date().getTime())
-  if (ms >= 0) {
-    return {
-      'dd': Math.floor(ms / (1000 * 60 * 60 * 24)),
-      'hh': fillZero(Math.floor(ms / (1000 * 60 * 60)) % 24),
-      'mm': fillZero(Math.floor(ms / (1000 * 60)) % 60),
-      'ss': fillZero(Math.floor(ms / 1000) % 60)
-    };
-  } else {
-    return false
-  }
-}
-// 随机ID
-export function randomID() {
-  return Math.random().toString(36).substring(2)
-}
-// 千分位数字
-export function thousands(num) {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-}
-// 获取随机数
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-// 随机生成颜色码
-export function getRandomColor() {
-  return '#' + (function (h) {
-    return new Array(7 - h.length).join('0') + h
-  })((Math.random() * 0x1000000 << 0).toString(16))
-}
-// uuid
-export function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    let r = Math.random() * 16 | 0,
-        v = c === 'x' ? r : (r & 0x3 | 0x8)
-    return v.toString(16)
-  })
-}
-// 数组乱序
-export function shuffle(arr) {
-  let _arr = arr.slice()
-  for (let i = 0, l = _arr.length; i < l; i++) {
-    let j = getRandom(0, i), t = _arr[i]
-    _arr[i] = _arr[j]
-    _arr[j] = t
-  }
-  return _arr
 }
 // 防抖
 export function debounce(func, delay) {
@@ -258,49 +457,6 @@ export function debounce(func, delay) {
       func.apply(this, args)
     }, delay)
   }
-}
-// 数组求和
-export function arraySum(arr) {
-  if (!arr.length) {
-    return 0
-  } else if (arr.length === 1) {
-    return arr[0] - 0
-  } else {
-    return arr[0] - 0 + arraySum(arr.slice(1))
-  }
-}
-// 求数组最大值
-export function arrayMax(arr) {
-  return Math.max(...arr)
-}
-// 求数组最小值
-export function arrayMin(arr) {
-  return Math.min(...arr)
-}
-// 数组切割
-export function arrayChunk(arr, size) {
-  return Array.from({length: Math.ceil(arr.length / size)}, (v, i) => arr.slice(i * size, i * size + size))
-}
-// 过滤数组中false值
-export function arrayCompact(arr) {
-  return arr.filter(Boolean)
-}
-// 计算数组中值的出现次数
-export function arrayCountOccurrences(arr, value) {
-  return arr.reduce((a, v) => v === value ? a + 1 : a + 0, 0)
-}
-// 深度拼合数组
-export function deepFlatten(arr) {
-  return [].concat(...arr.map(v => Array.isArray(v) ? deepFlatten(v) : v))
-}
-// 返回两个数组间的差异
-export function arrayDiff(a, b) {
-  const s = new Set(b)
-  return a.filter(v => !s.has(v))
-}
-// 初始化指定范围内的数字数组
-export function initArrayWithRange(end, start = 0) {
-  return Array.from({length: end - start + 1}).map((v, i) => i + start)
 }
 // 从对象中选取对应的键值对
 export function pick(obj, arr) {
@@ -335,63 +491,6 @@ export function deepCopy(values) {
     return copy
   }
 }
-// 时间戳转字符串
-export function timeToStr(timestamp, type = 'time') {
-  if (!timestamp) return '-'
-  let dt = new Date(timestamp)
-    , year = dt.getFullYear()
-    , month = fillZero(dt.getMonth() + 1)
-    , day = fillZero(dt.getDate())
-    , hour = fillZero(dt.getHours())
-    , minute = fillZero(dt.getMinutes())
-    , second = fillZero(dt.getSeconds())
-  if (type === 'time') {
-    return `${year}-${month}-${day} ${hour}:${minute}:${second}`
-  } else if (type === 'date') {
-    return `${year}-${month}-${day}`
-  }
-}
-// 计算两个日期之间相差天数，xxxx-xx-xx格式
-export function dateDiff(startDate, endDate) {
-  let startTime = new Date(Date.parse(startDate.replace(/-/g, '/'))).getTime()
-    , endTime = new Date(Date.parse(endDate.replace(/-/g, '/'))).getTime()
-    , dates = Math.abs((startTime - endTime)) / (1000 * 60 * 60 * 24)
-  return dates
-}
-// 获得字符长度
-export function getByteLen(val) {
-  let len = 0
-  for (let i = 0, l = val.length; i < l; i++) {
-    let a = val.charAt(i)
-    if (a.match(/[^\x00-\xff]/ig) != null) {
-      len += 2
-    } else {
-      len += 1
-    }
-  }
-  return len
-}
-// 日期格式化
-Date.prototype.Format = function (format) {
-  let o = {
-    'M+': this.getMonth() + 1, // month
-    'd+': this.getDate(), // day
-    'h+': this.getHours(), // hour
-    'm+': this.getMinutes(), // minute
-    's+': this.getSeconds(), // second
-    'q+': Math.floor((this.getMonth() + 3) / 3), // quarter
-    'S': this.getMilliseconds() // millisecond
-  }
-  if (/(y+)/.test(format)) {
-    format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length))
-  }
-  for (let k in o) {
-    if (new RegExp('(' + k + ')').test(format)) {
-      format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length))
-    }
-  }
-  return format
-}
 // 获得密码强度
 export function pwdIntensity(pwd) {
   let intensity = 0
@@ -412,61 +511,6 @@ export function pwdIntensity(pwd) {
   }
   return intensity
 }
-// 随机字符串生成
-export function randomString(len) {
-  len = len || 32;
-  let $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
-  let maxPos = $chars.length
-  let str = ''
-  for (let i = 0; i < len; i++) {
-    str += $chars.charAt(Math.floor(Math.random() * maxPos))
-  }
-  return str
-}
-// 匹配整数
-export function isInteger(val) {
-  return /^(-|\+)?\d+$/.test(val)
-}
-// 获取窗口可视范围的高度
-export function getClientHeight() {
-  let clientHeight = 0, doc = document
-  if (doc.body.clientHeight && doc.documentElement.clientHeight) {
-    clientHeight = (doc.body.clientHeight < doc.documentElement.clientHeight) ? doc.body.clientHeight : doc.documentElement.clientHeight
-  } else {
-    clientHeight = (doc.body.clientHeight > doc.documentElement.clientHeight) ? doc.body.clientHeight : doc.documentElement.clientHeight
-  }
-  return clientHeight
-}
-// 获取滚动条距顶部高度
-export function getScrollTop() {
-  return document.documentElement.scrollTop || document.body.scrollTop
-}
-// 设置滚动条距顶部高度
-export function setScrollTop(y) {
-  window.scrollTo(0, y)
-}
-// 获取当前滚动条位置
-export function getScrollPosition() {
-  let win = window
-  return {
-    x: win.pageXOffset !== undefined ? win.pageXOffset : win.scrollLeft,
-    y: win.pageYOffset !== undefined ? win.pageYOffset : win.scrollTop
-  }
-}
-// 滚动到指定元素区域
-export function smoothScroll(element) {
-  document.querySelector(element).scrollIntoView({
-    behavior: 'smooth'
-  })
-}
-// 平滑滚动到页面顶部
-export function scrollToTop() {
-  const top = document.documentElement.scrollTop || document.body.scrollTop
-  if (top > 0) {
-    window.requestAnimationFrame(scrollToTop)
-    window.scrollTo(0, top - top / 8)
-  }
-}
 // 转文字大小字节到KB,MB,GB并保留两位小数
 export function formatFileSize(fileSize) {
   let size
@@ -486,24 +530,6 @@ export function formatFileSize(fileSize) {
     return size + 'GB'
   }
 }
-// 判断元素是否在视口中
-export function elementIsVisibleInViewport(el) {
-  const { top, left, bottom, right } = el.getBoundingClientRect()
-  const { innerHeight, innerWidth } = window
-  return ((top > 0 && top < innerHeight) || (bottom > 0 && bottom < innerHeight)) && ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
-}
-// 时间戳转化为多久前
-export function timeFormat(timestamp) {
-  let mistiming = Math.round((Date.now() - timestamp) / 1000)
-    , arrr = ['年', '个月', '星期', '天', '小时', '分钟', '秒']
-    , arrn = [31536000, 2592000, 604800, 86400, 3600, 60, 1]
-  for (let i = 0; i < arrn.length; i++) {
-    let inm = Math.floor(mistiming / arrn[i])
-    if (inm != 0) {
-      return inm + arrr[i] + '前'
-    }
-  }
-}
 // 根据身份证获取年龄
 export function IdCard(card) {
   let _dt = new Date()
@@ -516,7 +542,7 @@ export function IdCard(card) {
   return age
 }
 // 复制字符串到剪切板
-export function copyString(str) {
+export function copyToClipboard(str) {
   const doc = document
     , el = doc.createElement('textarea')
   el.value = str
@@ -569,33 +595,6 @@ export function chainAsync(fns) {
   let curr = 0
   const next = () => fns[curr++](next)
   next()
-}
-// 驼峰转字符串
-export function fromCamelCase(str, separator = ' ') {
-  return str.replace(/([a-zd])([A-Z])/g, '$1' + separator + '$2')
-  .replace(/([A-Z]+)([A-Z][a-zd]+)/g, '$1' + separator + '$2').toLowerCase()
-}
-// 字符串转驼峰
-export function toCamelCase(str) {
-  let s = str.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g).map(x => x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase()).join('')
-  return s.slice(0, 1).toLowerCase() + s.slice(1)
-}
-// 复制到剪切版
-export function copyToClipboard(str) {
-  const el = document.createElement('textarea')
-  el.value = str
-  el.setAttribute('readonly', '')
-  el.style.position = 'absolute'
-  el.style.left = '-9999px'
-  document.body.appendChild(el)
-  const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false
-  el.select()
-  document.execCommand('copy')
-  document.body.removeChild(el)
-  if (selected) {
-    document.getSelection().removeAllRanges()
-    document.getSelection().addRange(selected)
-  }
 }
 // 转义html
 export function escapeHTML(str) {
@@ -692,6 +691,29 @@ let cookies = {
     }
     return aKeys
   }
+}
+
+console.obj = function (...args) {
+  let newArgs = args.map((item) => {
+      if (Object.prototype.toString.call(item) === '[object Object]' || Array.isArray(item)) {
+          return deepClone(item)
+      } else {
+          return item
+      }
+  })
+  console.log(...newArgs)
+}
+
+console.str = function (...args) {
+  let newArgs = args.map((item) => {
+      try {
+          let obj = JSON.stringify(item)
+          return obj
+      } catch(e) {
+          return item
+      }
+  })
+  console.log(...newArgs)
 }
 
 export {
