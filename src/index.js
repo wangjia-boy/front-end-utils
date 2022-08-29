95ae05bbc40b30ef5a7fecab59abf688dde3dbd6
best-wangjia <wangjia125513819@foxmail.com>
2022-08-29 23:35:44 +0800

    modify:提取storage

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import './css/base.styl'
-import { typeJudge, timeFormat, arrayChunk, getRandomHexColor,copyToClipboard2 } from './js/utils.js'
-
-console.log(typeJudge([1,2]))
-console.log(timeFormat(1656345124534))
-console.log(arrayChunk([2, 6, 87, 1, -1, 0, -876], 2))
-console.log(getRandomHexColor())
-console.log(new Date().Format('yyyy-MM-dd hh:mm:ss'))
-
-window.onload = function() {
-  let btn = document.querySelector('#btn')
-  btn.onclick = function() {
-    console.log(123)
-    copyToClipboard2('asd123zxc')
-  }
-}
+import { typeJudge } from './js/utils.js'
 
 new Vue({
-  el: '#app',
+  el: '#root',
   data: {
-    transfromAddr: false
-  }  
+    list: []
+  },
+  methods: {
+    input(e) {
+      let val3 = e.target.value
+      if (val3.length === 6) {
+        this.list.push(val3)
+        e.target.value = ''
+      }
+    }
+  }
 })
---
+++ src/js/utils/storage.js
@@ -0,0 +1,154 @@
+const lStorage = (function() {
+  const name = 'lStorage'
+    , storage = window.localStorage
+
+  const setItem = function (key, value) {
+    let data = storage.getItem(name)
+    if (!data) {
+      this.init()
+      data = storage.getItem(name)
+    }
+    data = JSON.parse(data)
+    data.data[key] = value
+    storage.setItem(name, JSON.stringify(data))
+    return data.data
+  }
+
+  const getItem = function (key) {
+    let data = storage.getItem(name)
+    if (!data) {
+      return false
+    }
+    data = JSON.parse(data)
+    return data.data[key]
+  }
+
+  const removeItem = function (key) {
+    let data = storage.getItem(name)
+    if (!data) {
+      return false
+    }
+    data = JSON.parse(data)
+    delete data.data[key]
+    storage.setItem(name, JSON.stringify(data))
+    return data.data
+  }
+
+  const clear = function () {
+    storage.removeItem(name)
+  }
+
+  const init = function () {
+    storage.setItem(name, '{"data":{}}')
+  }
+
+  return { test, setItem, getItem, removeItem, init, clear }
+
+})()
+
+const sStorage = (function() {
+  const name = 'sStorage'
+    , storage = window.sessionStorage
+
+  const setItem = function (key, value) {
+    let data = storage.getItem(name)
+    if (!data) {
+      this.init()
+      data = storage.getItem(name)
+    }
+    data = JSON.parse(data)
+    data.data[key] = value
+    storage.setItem(name, JSON.stringify(data))
+    return data.data
+  }
+
+  const getItem = function (key) {
+    let data = storage.getItem(name)
+    if (!data) {
+      return false
+    }
+    data = JSON.parse(data)
+    return data.data[key]
+  }
+
+  const removeItem = function (key) {
+    let data = storage.getItem(name)
+    if (!data) {
+      return false
+    }
+    data = JSON.parse(data)
+    delete data.data[key]
+    storage.setItem(name, JSON.stringify(data))
+    return data.data
+  }
+
+  const clear = function () {
+    storage.removeItem(name)
+  }
+
+  const init = function () {
+    storage.setItem(name, '{"data":{}}')
+  }
+
+  return { test, setItem, getItem, removeItem, init, clear }
+
+})()
+
+const cookies = (function() {
+  const getItem = function (key) {
+    if (!key) {
+      return null
+    }
+    return decodeURIComponent(document.cookie.replace(new RegExp('(?:(?:^|.*;)\\s*' + encodeURIComponent(key).replace(/[\-\.\+\*]/g, '\\$&') + '\\s*\\=\\s*([^;]*).*$)|^.*$'), '$1')) || null
+  }
+
+  const setItem = function (key, value, end, path, domain, secure) {
+    if (!key || /^(?:expires|max\-age|path|domain|secure)$/i.test(key)) {
+      return false
+    }
+    let expires = ''
+    if (end) {
+      switch (end.constructor) {
+        case Number:
+          expires = end === Infinity ? '; expires=Fri, 31 Dec 9999 23:59:59 GMT' : '; max-age=' + end
+          break
+        case String:
+          expires = '; expires=' + end
+          break
+        case Date:
+          expires = '; expires=' + end.toUTCString()
+          break
+      }
+    }
+    document.cookie = encodeURIComponent(key) + '=' + encodeURIComponent(value) + expires + (domain ? '; domain=' + domain : '') + (path ? '; path=' + path : '') + (secure ? '; secure' : '')
+    return true
+  }
+
+  const removeItem = function (key, path, domain) {
+    if (!this.hasItem(key)) {
+      return false
+    }
+    document.cookie = encodeURIComponent(key) + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT' + (domain ? '; domain=' + domain : '') + (path ? '; path=' + path : '')
+    return true
+  }
+
+  const hasItem = function (key) {
+    if (!key) {
+      return false
+    }
+    return (new RegExp('(?:^|;\\s*)' + encodeURIComponent(key).replace(/[\-\.\+\*]/g, '\\$&') + '\\s*\\=')).test(document.cookie)
+  }
+
+  const keys = function () {
+    let keys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, '').split(/\s*(?:\=[^;]*)?;\s*/)
+    for (let nLen = keys.length, nIdx = 0; nIdx < nLen; nIdx++) {
+      keys[nIdx] = decodeURIComponent(keys[nIdx])
+    }
+    return keys
+  }
+
+  return { getItem, setItem, removeItem, hasItem, keys }
+
+})
+
+export default { lStorage, sStorage, cookies }
